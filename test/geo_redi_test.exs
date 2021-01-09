a9e4b9fba156f664860c78fe1a1ea6619ec94e4e
k <rmariana@gmail.com>
2021-01-09 17:43:16 +0100

    preliminary tests

--- test/geo_redi_test.exs
+++ test/geo_redi_test.exs
@@ -3,11 +3,30 @@ defmodule GeoRediTest do
   doctest GeoRedi
 
   test "no data in cache" do
-    assert GeoRedi.get_addr(
-             59.496146587425265,
-             0.12258659847596874,
-             fn _, _ -> "undefined" end,
-             "undefined"
-           ) == "undefined"
+    inc = 0.000001
+
+    lat = 1.00001
+    lng = 2.00001
+    addr = fallback lat, lng
+
+    assert GeoRedi.get_addr(lat, lng, &fallback/2, nil) == addr
+    IO.inspect :ets.tab2list( :addr), label: "addr 1"
+
+    assert GeoRedi.get_addr(lat+inc, lng, &fallback/2, nil) == addr
+    IO.inspect :ets.tab2list( :addr), label: "addr 2"
+
+    assert GeoRedi.get_addr(lat, lng+inc, &fallback/2, nil) == addr
+    IO.inspect :ets.tab2list( :addr), label: "addr 3"
+    num_items =  GeoRedi.rebuild_live_cache()
+    IO.puts "rebuild_live_cache #{num_items}"
+    IO.inspect :ets.tab2list( :latlng), label: "latlng"
+    IO.inspect :ets.tab2list( :addr), label: "addr"
   end
+
+  def fallback(lat,lng) when is_float(lat) and is_float(lng) do
+    Process.sleep(100); "ADDR_#{decimal_5(lat)}_#{decimal_5(lng)}"
+  end
+
+  def decimal_5(val), do: :erlang.float_to_binary(val, decimals: 4)
+
 end
